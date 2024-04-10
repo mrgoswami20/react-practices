407897fdfc61f5cda4ea14d9938d7cfef4c367e7
Rohit Kumar Goswami <105840434+mrgoswami20@users.noreply.github.com>
2024-04-11 01:16:33 +0530

    day-2 practice done

--- Dice-Game/src/components/GamePage.jsx
+++ Dice-Game/src/components/GamePage.jsx
@@ -3,38 +3,80 @@ import Button from "./Button";
 import diceMapper from "./shared/DiceMapper";
 
 const GamePage = () => {
-    const arrayNum = [1,2,3,4,5,6];
-    const [chooseNumber, setChooseNumber] = useState();
+    const arrayNum = [1, 2, 3, 4, 5, 6];
+    const [clicked, setClicked] = useState(-1);
+    const [diceImage, setDiceImage] = useState(diceMapper[1]);
+    const [showScore, setShowScore] = useState(0);
+    const [showWarn, setShowWarn] = useState(false);
+    const[showRules, setShowRules] = useState(false);
+
+    function setChooseNumber(value) {
+        setClicked(value);
+        setShowWarn(false);
+    }
+
+    function diceImageHandler() {
+        let randNum = Math.floor(Math.random() * 6);
+        setDiceImage(diceMapper[randNum + 1]);
+        console.log(diceMapper[randNum+ 1])
+        if (clicked == randNum) setShowScore((prev) => prev + clicked);
+        else setShowScore((prev) => prev - 2);
+    }
+
+
 
   return (
-    <div className="p-20 min-h-screen">
-        <div className="flex justify-between">
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-7xl">0</h1>
-                <p className="text-lg font-[700]">Total Score</p>
-            </div>
-            <div className="flex flex-col items-end">
-                <p chooseNumber={false} className="text-red-600">You have not selected any number</p>
-                <label className="flex max-h-[72px] ">
-                    {arrayNum.map((value, i)=>(
-                        <input className="border-solid border-2 m-2 px-5 p-4 cursor-pointer"
-                            key ={i} onClick={()=>setChooseNumber(value)}
-                            type="radio"
-                        />
-                    ))}
-                </label>
-                <p className=" font-[900]">Select Number</p>
-            </div>
+    <div className="p-16 min-h-screen">
+      <div className="flex justify-between">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-7xl">{showScore}</h1>
+          <p className="text-lg font-[700]">Total Score</p>
         </div>
-        <div className="flex flex-col items-center justify-center pt-10 gap-2">
-            <img src={diceMapper[1]} alt="" className=" max-w-52 cursor-pointer"/>
-            <p>click on Dice to roll</p>
-            <Button text="Reset Score" type="secondary"/>
-            <Button text="Show Rules" type="primary"/>
-            <div className=" bg-green-400 ">
-                rules
-            </div>
+        <div className="flex flex-col items-end">
+          {showWarn ? (
+            <p className="text-red-600">You have not selected any number</p>
+          ) : null}
+          <div className="flex max-h-[72px] ">
+            {arrayNum.map((value, i) =>
+              (
+                <button
+                  className={`border-solid border-2 m-2 px-5 p-4 cursor-pointer ${value===clicked && 'bg-black text-white'}`}
+                  key={i}
+                  onClick={()=>setChooseNumber(value) }
+                >
+                  {value}
+                </button> 
+              )
+            )}
+          </div>
+          <p className="font-[900]">Select Number</p>
         </div>
+      </div>
+      <div className="flex flex-col items-center justify-center pt-10 gap-2">
+        <img
+          src={diceImage}
+          alt="not-loaded"
+          onClick={
+            clicked < 0
+              ? () => setShowWarn(true)
+              : () => diceImageHandler()
+          }
+          className=" max-w-52 cursor-pointer active:scale-90"
+        />
+        <p>click on Dice to roll</p>
+        <Button text="Reset Score" type="secondary" />
+        <Button text="Show Rules" onClick={()=>setShowRules((prev)=>!prev)} type="primary" />
+        {showRules ? <div className="flex flex-col justify-center items-start max-h-48 bg-purple-100 p-4 mt-3 ">
+          <p className="font-semibold text-xl pb-1">How to play dice game</p>
+          <p className=" text-xs font-medium">
+            Select any number <br />
+            Click on dice image <br />
+            after click on dice if selected number is equal to dice number you
+            will get same point as dice <br />
+            if you get wrong guess then 2 point will be dedcuted
+          </p>
+        </div>: null}
+      </div>
     </div>
   );
 };
--- Dice-Game/src/components/shared/DiceMapper.js
+++ Dice-Game/src/components/shared/DiceMapper.js
@@ -1,10 +1,21 @@
-import Dice1 from "../../assets/dice_1.png"
-import Dice2 from "../../assets/dice_2.png"
-import Dice3 from "../../assets/dice_3.png"
-import Dice4 from "../../assets/dice_4.png"
-import Dice5 from "../../assets/dice_5.png"
-import Dice6 from "../../assets/dice_6.png"
+import Dice1 from "../../assets/dice_1.png";
+import Dice2 from "../../assets/dice_2.png";
+import Dice3 from "../../assets/dice_3.png";
+import Dice4 from "../../assets/dice_4.png";
+import Dice5 from "../../assets/dice_5.png";
+import Dice6 from "../../assets/dice_6.png";
 
-const diceMapper = {1:Dice1, 2:Dice2, 3:Dice3, 4:Dice4, 5:Dice5, 6:Dice6};
+const diceMapper = {
+  1: Dice1,
+  2: Dice2,
+  3: Dice3,
+  4: Dice4,
+  5: Dice5,
+  6: Dice6,
+};
 
 export default diceMapper;
+
+
+
+
